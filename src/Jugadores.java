e2c2aa9a3ddfac50b8868c73c62ea54174478c9c
Alumno-tarde <daniel.iturralde.26@gmail.com>
2023-03-23 16:57:54 +0100

    examen

--- src/Jugadores.java
+++ src/Jugadores.java
@@ -1,54 +1,92 @@
 
 public class Jugadores {
 
-	int ID ;
-	String Nombre, Equipo;
-	int Dorsal;
-	double Altura;
+	public int ID ;
+	public static String Nombre;
+	public String Equipo;
+	public int Dorsal;
+	public double Altura;
 	/**
 	 * @param iD
-	 * @param nombre
 	 * @param equipo
 	 * @param dorsal
 	 * @param altura
 	 */
-	public Jugadores( String nombre, String equipo, int dorsal, double altura) {
+	public Jugadores(int iD, String nombre, String equipo, int dorsal, double altura) {
 		super();
-		
+		ID = iD;
 		Nombre = nombre;
 		Equipo = equipo;
 		Dorsal = dorsal;
 		Altura = altura;
-		
 	}
+	/**
+	 * @return the iD
+	 */
 	public int getID() {
 		return ID;
 	}
+	/**
+	 * @param iD the iD to set
+	 */
 	public void setID(int iD) {
 		ID = iD;
 	}
+	/**
+	 * @return the nombre
+	 */
 	public String getNombre() {
 		return Nombre;
 	}
+	/**
+	 * @param nombre the nombre to set
+	 */
 	public void setNombre(String nombre) {
 		Nombre = nombre;
 	}
+	/**
+	 * @return the equipo
+	 */
 	public String getEquipo() {
 		return Equipo;
 	}
+	/**
+	 * @param equipo the equipo to set
+	 */
 	public void setEquipo(String equipo) {
 		Equipo = equipo;
 	}
+	/**
+	 * @return the dorsal
+	 */
 	public int getDorsal() {
 		return Dorsal;
 	}
+	/**
+	 * @param dorsal the dorsal to set
+	 */
 	public void setDorsal(int dorsal) {
 		Dorsal = dorsal;
 	}
+	/**
+	 * @return the altura
+	 */
 	public double getAltura() {
 		return Altura;
 	}
+	/**
+	 * @param altura the altura to set
+	 */
 	public void setAltura(double altura) {
 		Altura = altura;
 	}
+	public int getID(Jugadores jugadorID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public String toString() {
+		return "Jugadores [ID=" + ID + ", Equipo=" + Equipo + ", Dorsal=" + Dorsal + ", Altura=" + Altura + "]";
+	}
 }
+	
--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,12 @@
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Iterator;
-
-import javax.management.openmbean.OpenDataException;
-import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-
-
+import javax.swing.JOptionPane;
 
 public class Menu {
 
@@ -17,80 +15,190 @@ public class Menu {
 		menu();
 	}
 
-	public static int menu() {
-		int respuesta = 0;
-		int Dorsal =0;
-		double Altura = 0.0;
-		
-		while (respuesta == 0) {
-			try {
-				String opcion = JOptionPane.showInputDialog("Introduce una opción \n 1- Añadir \n 2- Listar \n 3- Borrar");
-				respuesta = Integer.parseInt(opcion);
+	// INSERTARJUGADOR
+	public static boolean insertarjugador(Jugadores Nuevojugador) throws ClassNotFoundException {
+		PreparedStatement ps = null;
 
-				
-				
-//				Opcion1				
-				if (opcion.equals(1)); {
-//					Añadir Nombre
-					String AñadirNombre = JOptionPane.showInputDialog("Añadir Nombre del Jugador");
-					JOptionPane.showMessageDialog(null, "Nombre"+""+(AñadirNombre)+""+"añadido");
-				
-					
-//					AñadirEquipo
-					String AñadirEquipo = JOptionPane.showInputDialog("Añadir Equipo del Jugador");
-					JOptionPane.showMessageDialog(null, "Equipo"+""+(AñadirEquipo)+""+"añadido");
-					
-					
-//					AñadirDorsal
-					String AñadirDorsal = JOptionPane.showInputDialog("Añadir Dorsal del Jugador");
-					JOptionPane.showMessageDialog(null, "Dorsal"+""+(AñadirDorsal)+""+"añadido");
-					AñadirDorsal.valueOf(Dorsal);
-					
-					
-//					Añadir Altura
-					String AñadirAltura = JOptionPane.showInputDialog("Añadir Altura del Jugador");
-					JOptionPane.showMessageDialog(null, "Altura"+""+(AñadirAltura)+""+"añadido");					
-					AñadirAltura.valueOf(Altura);
-					
-//					Nuevo Jugador
-					Jugadores jugador = new Jugadores(AñadirNombre, AñadirEquipo, Dorsal, Altura);
-					
-					jugador.setNombre(AñadirNombre);
-					jugador.setEquipo(AñadirEquipo);
-					jugador.setDorsal(Dorsal);
-					jugador.setAltura(Altura);
-					insertarjugador(jugador);
-					JOptionPane.showMessageDialog(null, "El jugador"+""+ (AñadirNombre)+"" +"ha sido Añadido" );
-				}
+		Class.forName("org.sqlite.JDBC");
+		try (Connection cx = DriverManager.getConnection(
+				"jdbc:sqlite:C:\\Users\\Alumno-tarde\\Desktop\\Daniel Iturralde\\Eclipse\\Examen1\\Sistema\\sistema3")) {
 
-				
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "ERROR");
-			}
-		}
-		return respuesta;
-	}
-
-	public static boolean insertarjugador(Jugadores jugador) throws ClassNotFoundException {
-		PreparedStatement ps=null;
-		try {
-			
-			Class.forName("org.sqlite.JDBC");
-			Connection cx=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Alumno-tarde\\Desktop\\Daniel Iturralde\\Eclipse\\Examen1\\Sistema\\sistema");
-			
-			ps=cx.prepareStatement("INSERT INTO Jugadores VALUES(NULL,?,?,?,?)");
-			ps.setString(1, jugador.getNombre());
-			ps.setString(2, jugador.getEquipo());
-			ps.setInt(3, jugador.getDorsal());
-			ps.setDouble(4, jugador.getAltura());
+			ps = cx.prepareStatement("INSERT INTO Jugadores VALUES(?,?,?,?,?)");
+			ps.setInt(1, Localizadorid() + 1);
+			ps.setString(2, Nuevojugador.getNombre());
+			ps.setString(3, Nuevojugador.getEquipo());
+			ps.setInt(4, Nuevojugador.getDorsal());
+			ps.setDouble(5, Nuevojugador.getAltura());
 			ps.executeUpdate();
 			JOptionPane.showMessageDialog(null, "Se ha agregado correctamente");
-			cx.close();
 			return true;
 		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "No se ha podido agregar");
 			e.printStackTrace();
 			return false;
 		}
+
 	}
+
+	// BORRAR JUGADOR
+	public static boolean BorrarJugador(int ID) throws ClassNotFoundException {
+		PreparedStatement ps = null;
+		Class.forName("org.sqlite.JDBC");
+		try(Connection cx = DriverManager.getConnection(
+				"jdbc:sqlite:C:\\Users\\Alumno-tarde\\Desktop\\Daniel Iturralde\\Eclipse\\Examen1\\Sistema\\sistema3")) {
+			
+			
+			ps = cx.prepareStatement("DELETE FROM Jugadores WHERE ID=?");
+			ps.setInt(1, ID);
+			ps.executeUpdate();
+			JOptionPane.showMessageDialog(null, "Se ha BORRADO correctamente");
+			
+			return true;
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "No se ha BORRADO correctamente");
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+//	SACAR id DEVUELVE UN ID
+	public static int Localizadorid() throws ClassNotFoundException {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Class.forName("org.sqlite.JDBC");
+		
+		try(Connection cx = DriverManager.getConnection(
+				"jdbc:sqlite:C:\\Users\\Alumno-tarde\\Desktop\\Daniel Iturralde\\Eclipse\\Examen1\\Sistema\\sistema3")) {
+			
+			ps = cx.prepareStatement("SELECT MAX(ID) FROM Jugadores");
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				return rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "ERROR Al Sacar el ID");
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
+
+//	MOSTRAR JUGADORES (DA FALLO EN CONSULTARJUGADORES)
+	public static ArrayList<Jugadores> consultaJugadores() throws ClassNotFoundException {
+		ArrayList<Jugadores> lista = new ArrayList<Jugadores>();
+//		Iterator<Jugadores> IDIterator = lista.iterator();
+//		while(IDIterator.hasNext()) {
+//			Jugadores Mostrar= IDIterator.next();
+//			System.out.print(Mostrar+" / ");
+//			
+//		}
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Class.forName("org.sqlite.JDBC");
+		try(Connection cx = DriverManager.getConnection(
+				"jdbc:sqlite:C:\\Users\\Alumno-tarde\\Desktop\\Daniel Iturralde\\Eclipse\\Examen1\\Sistema\\sistema3")) {
+			
+			
+			ps = cx.prepareStatement("SELECT * FROM Jugadores ");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Jugadores jugadorAMostrar = new Jugadores(0, null, null, 0, 0.0);
+				jugadorAMostrar.setID(rs.getInt("id"));
+				jugadorAMostrar.setNombre(rs.getString("Nombre"));
+				jugadorAMostrar.setEquipo(rs.getString("Equipo"));
+				jugadorAMostrar.setDorsal(rs.getInt("Numero"));
+				jugadorAMostrar.setAltura(rs.getDouble("Altura"));
+				lista.add(jugadorAMostrar);
+				
+			}
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "consultarJugadores no ha funcionado");
+			e.printStackTrace();
+		}
+		return lista;
+
+	}
+
+	public static int menu() {
+		int respuesta = 0;
+		int Dorsal = 0;
+		double Altura = 0.0;
+		Integer eliminar = 0;
+
+		while (respuesta == 0) {
+			try {
+				String opcion = JOptionPane
+						.showInputDialog("Introduce una opción \n 1- Añadir \n 2- Listar \n 3- Borrar");
+				respuesta = Integer.parseInt(opcion);
+
+//				Opcion1				
+				try {
+					if (opcion.equals("1")) {
+//					Añadir Nombre
+						String AñadirNombre = JOptionPane.showInputDialog("Añadir Nombre del Jugador");
+						JOptionPane.showMessageDialog(null, "Nombre" + " " + (AñadirNombre) + " " + "añadido");
+
+//					AñadirEquipo
+						String AñadirEquipo = JOptionPane.showInputDialog("Añadir Equipo del Jugador");
+						JOptionPane.showMessageDialog(null, "Equipo" + " " + (AñadirEquipo) + " " + "añadido");
+
+//					AñadirDorsal
+						String AñadirDorsal = JOptionPane.showInputDialog("Añadir Dorsal del Jugador");
+						JOptionPane.showMessageDialog(null, "Dorsal" + " " + (AñadirDorsal) + " " + "añadido");
+						Dorsal = Integer.valueOf(AñadirDorsal);
+
+//					Añadir Altura
+						String AñadirAltura = JOptionPane.showInputDialog("Añadir Altura del Jugador");
+						JOptionPane.showMessageDialog(null, "Altura" + " " + (AñadirAltura) + " " + "añadido");
+						Altura = Double.valueOf(AñadirAltura);
+
+//					Nuevo Jugador
+						Jugadores Nuevojugador = new Jugadores(Localizadorid(), AñadirNombre, AñadirEquipo, Dorsal,
+								Altura);
+						Nuevojugador.setNombre(AñadirNombre);
+						Nuevojugador.setEquipo(AñadirEquipo);
+						Nuevojugador.setDorsal(Dorsal);
+						Nuevojugador.setAltura(Altura);
+						insertarjugador(Nuevojugador);
+						JOptionPane.showMessageDialog(null,
+								"El jugador" + " " + (AñadirNombre) + " " + "ha sido Añadido");
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "No se ha podido escribir el jugador");
+				}
+
+//				LISTAR
+				try {
+					if (opcion.equals("2")) {
+						consultaJugadores();
+						JOptionPane.showMessageDialog(null, consultaJugadores() + " / ");
+
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "No se ha podido Listar a los jugadores");
+				}
+
+//				BORRAR JUGADOR
+				try {
+					if (opcion.equals("3")) {
+						String Eliminar = JOptionPane.showInputDialog("Vas a Eliminar a un Jugador");
+						eliminar = Integer.valueOf(Eliminar);
+						BorrarJugador(eliminar);
+
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "No se ha podido escribir el Borrar jugador");
+				}
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Fallo en el Menu");
+			}
+		}
+		return respuesta;
+	}
+
 }
