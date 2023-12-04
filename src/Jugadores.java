4187cb49339395ef5d64a780928d8f4c3cc76136
Drakens90 <126493580+Drakens90@users.noreply.github.com>
2023-12-05 00:54:53 +0100

    Jugadores.java

--- src/Jugadores.java
+++ src/Jugadores.java
@@ -1,92 +1,53 @@
-
 public class Jugadores {
 
-	public int ID ;
-	public static String Nombre;
-	public String Equipo;
-	public int Dorsal;
-	public double Altura;
-	/**
-	 * @param iD
-	 * @param equipo
-	 * @param dorsal
-	 * @param altura
-	 */
-	public Jugadores(int iD, String nombre, String equipo, int dorsal, double altura) {
-		super();
-		ID = iD;
-		Nombre = nombre;
-		Equipo = equipo;
-		Dorsal = dorsal;
-		Altura = altura;
-	}
-	/**
-	 * @return the iD
-	 */
-	public int getID() {
-		return ID;
-	}
-	/**
-	 * @param iD the iD to set
-	 */
-	public void setID(int iD) {
-		ID = iD;
-	}
-	/**
-	 * @return the nombre
-	 */
-	public String getNombre() {
-		return Nombre;
-	}
-	/**
-	 * @param nombre the nombre to set
-	 */
-	public void setNombre(String nombre) {
-		Nombre = nombre;
-	}
-	/**
-	 * @return the equipo
-	 */
-	public String getEquipo() {
-		return Equipo;
-	}
-	/**
-	 * @param equipo the equipo to set
-	 */
-	public void setEquipo(String equipo) {
-		Equipo = equipo;
-	}
-	/**
-	 * @return the dorsal
-	 */
-	public int getDorsal() {
-		return Dorsal;
-	}
-	/**
-	 * @param dorsal the dorsal to set
-	 */
-	public void setDorsal(int dorsal) {
-		Dorsal = dorsal;
-	}
-	/**
-	 * @return the altura
-	 */
-	public double getAltura() {
-		return Altura;
-	}
-	/**
-	 * @param altura the altura to set
-	 */
-	public void setAltura(double altura) {
-		Altura = altura;
-	}
-	public int getID(Jugadores jugadorID) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public String toString() {
-		return "Jugadores [ID=" + ID + ", Equipo=" + Equipo + ", Dorsal=" + Dorsal + ", Altura=" + Altura + "]";
-	}
+    public final int ID;
+    public final String Nombre;
+    public final String Equipo;
+    public final int Dorsal;
+    public final double Altura;
+
+    /**
+     * Constructor de la clase Jugadores.
+     *
+     * @param ID     Identificador del jugador.
+     * @param nombre Nombre del jugador.
+     * @param equipo Equipo al que pertenece el jugador.
+     * @param dorsal Número de dorsal del jugador.
+     * @param altura Altura del jugador.
+     */
+    public Jugadores(int ID, String nombre, String equipo, int dorsal, double altura) {
+        this.ID = ID;
+        this.Nombre = nombre;
+        this.Equipo = equipo;
+        this.Dorsal = dorsal;
+        this.Altura = altura;
+    }
+
+    // Métodos de acceso
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getEquipo() {
+        return Equipo;
+    }
+
+    public int getDorsal() {
+        return Dorsal;
+    }
+
+    public double getAltura() {
+        return Altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugadores [ID=" + ID + ", Nombre=" + Nombre + ", Equipo=" + Equipo + ", Dorsal=" + Dorsal
+                + ", Altura=" + Altura + "]";
+    }
 }
-	
